c464153bbd3821dfdd18ae3cf518e366cdfd5a1b
Andreas Larsson <alarsson@williamsinteractive.com>
2014-04-30 15:39:58 +0200

    Implemented asynch execution of git

--- test/test.js
+++ test/test.js
@@ -1,10 +1,14 @@
-var gitw = require("../gitwalker.js");
+var gitw = require("../lib/gitwalker.js");
 
 exports.test1 = function(test) {
 
     test.expect(1);
-	gitw.walk("C:/dev/projects/wimii", "git status -sb", function() {
-    	test.ok(true, "this assertion should pass");
+   
+
+	//console.log(gitw);
+	
+	var gw = gitw.walk("C:/dev/projects/wimii", "echo", function() {
+		test.ok(true, "this assertion should pass");
 		test.done();		
 	});
 
